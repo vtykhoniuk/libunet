afb872f133afe8ab2d24f5de89b84c781bfd6a83
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-04-05 23:29:53 -0400

    Add SA_RESTART flag for non SIGALRM signals

--- signal.c
+++ signal.c
@@ -1,13 +1,18 @@
 #include "unet.h"
 
+#include <string.h>
 
-Action Signal(int signum, Action action)
+Action Signal(int signo, Action action)
 {
     struct sigaction act, oldact;
+    memset(&act, 0, sizeof(act));
 
     sigemptyset(&act.sa_mask);
     act.sa_handler = action;
-    if (sigaction(signum, &act, &oldact) == -1)
+    if (signo != SIGALRM)
+        act.sa_flags |= SA_RESTART;
+
+    if (sigaction(signo, &act, &oldact) == -1)
         err_sys("sigaction error");
 
     return oldact.sa_handler;
