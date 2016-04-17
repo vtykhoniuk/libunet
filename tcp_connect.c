cbdec2f2b2b67b45c487f304154be28d47164d41
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-04-17 13:34:59 -0400

    code refactoring

--- tcp_connect.c
+++ tcp_connect.c
@@ -4,20 +4,21 @@
 
 int tcp_connect(const char *host, const char *serv)
 {
-    int sockfd = -1;
-    struct addrinfo *res, hints;
-
+    struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_flags = AI_CANONNAME;
     hints.ai_socktype = SOCK_STREAM;
 
-    Getaddrinfo(host, serv, &hints, &res);
+    struct addrinfo *res;
+    int error;
+    if ((error = getaddrinfo(host, serv, &hints, &res)) == -1)
+        err_quit("tcp_connect error for host [%s] service [%s]: %s", host, serv, gai_strerror(error));
 
+    int sockfd;
     struct addrinfo *r = res;
     do {
-        sockfd = socket(r->ai_family, r->ai_socktype, r->ai_protocol);
-        if (sockfd == -1)
+        if ((sockfd = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1)
             continue;
 
         if (connect(sockfd, r->ai_addr, r->ai_addrlen) == 0)
@@ -25,6 +26,13 @@ int tcp_connect(const char *host, const char *serv)
 
         Close(sockfd);
     } while ((r = r->ai_next) != NULL);
+
+    /* We iterate though all addrinfo list and couldn't either
+       create a socket or bind. Print out latest sys error and
+       exit
+       */
+    if (r == NULL)
+        err_sys("tcp_connect error for host [%s] service [%s]", host, serv);
 
     freeaddrinfo(res);
 
